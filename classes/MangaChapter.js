570985b0cc64e463ba3848eec191cf9f77efa721
DaronV2 <darkenciel31@gmail.com>
2024-12-10 19:14:42 +0100

    🎨 Add chapter index

--- classes/MangaChapter.js
+++ classes/MangaChapter.js
@@ -6,10 +6,12 @@ class MangaChapter{
      * @constructor
      * @param {string} chapterName - The title of the chapter.
      * @param {Array<PageUrl>} listUrlOfChapter - The author of the book.
+     * @param {number} chapterIndex - The chapter index.
      */
     constructor(chapterName, listUrlOfChapter){
         this.chapterName = chapterName; // nom du chapitre
         this.listUrlOfChapter = listUrlOfChapter; // array des url d'un chapitre
+        this.chapterIndex = chapterIndex; // array des url d'un chapitre
     }
 }
 module.exports = MangaChapter;
